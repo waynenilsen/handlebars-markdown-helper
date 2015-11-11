0e0cb9720456bcc5b7fc23fbb3c9df73115b87e4
Wayne Nilsen <waynenilsen@gmail.com>
2015-11-11 11:41:31 -0500

    getting there

--- target/debug/handlebars_markdown_helper.d
+++
@@ -1,4 +0,0 @@
-/home/wayne/projects/handlebars-markdown-helper/target/debug/libhandlebars_markdown_helper.rlib: lib.rs
-
-/home/wayne/projects/handlebars-markdown-helper/target/debug/handlebars_markdown_helper.d: lib.rs
-
