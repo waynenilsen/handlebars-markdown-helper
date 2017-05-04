a2df7c304b2ed0e005f857f6ac36bfab72b1867e
Ning Sun <sunng@about.me>
2017-05-04 22:04:50 +0800

    (feat) update handlebars helper to 0.24+ API
    Signed-off-by: Ning Sun <sunng@about.me>

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,5 @@
-extern crate handlebars; 
-use handlebars::{
-	Context,
-	Handlebars,
-	JsonRender,
-	RenderContext,
-	RenderError,
-	Helper
-	};
+extern crate handlebars;
+use handlebars::{Handlebars, JsonRender, RenderContext, RenderError, Helper};
 
 extern crate pulldown_cmark;
 use self::pulldown_cmark::Parser;
@@ -19,31 +12,37 @@ pub fn render_html(text: String) -> String {
     s
 }
 
-pub fn markdown_helper(_: &Context, h: &Helper, _ : &Handlebars, rc: &mut RenderContext) -> Result<(), RenderError> {
-	let markdown_text_var = try!(h.param(0).ok_or_else(|| RenderError::new("Param not found for helper \"markdown\"")));
-	let markdown_text = markdown_text_var.value().render(); 
-	let html_string = render_html(markdown_text);
-	try!(rc.writer.write(html_string.into_bytes().as_ref()));
-	Ok(())
+pub fn markdown_helper(h: &Helper,
+                       _: &Handlebars,
+                       rc: &mut RenderContext)
+                       -> Result<(), RenderError> {
+    let markdown_text_var =
+        try!(h.param(0)
+                 .ok_or_else(|| RenderError::new("Param not found for helper \"markdown\"")));
+    let markdown_text = markdown_text_var.value().render();
+    let html_string = render_html(markdown_text);
+    try!(rc.writer.write(html_string.into_bytes().as_ref()));
+    Ok(())
 }
 
 #[cfg(test)]
 mod test {
-    use handlebars::{Template, Handlebars};
+    use handlebars::Handlebars;
     use std::collections::BTreeMap;
 
     #[test]
     fn test_markdown() {
-        let t0 = Template::compile("{{markdown x}}".to_string()).ok().unwrap();
+        let t0 = "{{markdown x}}";
 
         let mut handlebars = Handlebars::new();
         handlebars.register_helper("markdown", Box::new(::markdown_helper));
-        handlebars.register_template("t0", t0);
+        assert!(handlebars.register_template_string("t0", t0).is_ok());
 
-        let mut m :BTreeMap<String, String> = BTreeMap::new();
+        let mut m: BTreeMap<String, String> = BTreeMap::new();
         m.insert("x".into(), "# wow\n\n## second wow".into());
 
         let r0 = handlebars.render("t0", &m);
-        assert_eq!(r0.ok().unwrap(), "<h1>wow</h1>\n<h2>second wow</h2>\n".to_string());
+        assert_eq!(r0.ok().unwrap(),
+                   "<h1>wow</h1>\n<h2>second wow</h2>\n".to_string());
     }
 }
